9b5d38714c9ce6cdcd7c2f43e45b3d80e2645293
Pablo Duran da Maia <pablo.maia@alunos.sc.senac.br>
2021-06-17 17:12:20 -0300

    Projeto Pronto

---
+++ app.js
@@ -0,0 +1,70 @@
+var exibirsobre = function(){
+    divsobre = document.querySelector("#sobre");
+    divhabilidades = document.querySelector("#habilidades");
+    divformacao = document.querySelector("#formacao");
+    divhobbies = document.querySelector("#hobbies");
+    divbooks = document.querySelector("#books");
+
+    divsobre.style.display = "block";
+    divhabilidades.style.display = "none";
+    divformacao.style.display = "none";
+    divhobbies.style.display = "none";
+    divbooks.style.display = "none";
+}
+
+var exibirhabilidades = function(){
+    divsobre = document.querySelector("#sobre");
+    divhabilidades = document.querySelector("#habilidades");
+    divformacao = document.querySelector("#formacao");
+    divhobbies = document.querySelector("#hobbies");
+    divbooks = document.querySelector("#books");
+
+    divsobre.style.display = "none";
+    divhabilidades.style.display = "block";
+    divformacao.style.display = "none";
+    divhobbies.style.display = "none";
+    divbooks.style.display = "none";
+}
+
+var exibirformacao = function(){
+    divsobre = document.querySelector("#sobre");
+    divhabilidades = document.querySelector("#habilidades");
+    divformacao = document.querySelector("#formacao");
+    divhobbies = document.querySelector("#hobbies");
+    divbooks = document.querySelector("#books");
+
+    divsobre.style.display = "none";
+    divhabilidades.style.display = "none";
+    divformacao.style.display = "block";
+    divhobbies.style.display = "none";
+    divbooks.style.display = "none";
+}
+
+var exibirhobbies = function(){
+    divsobre = document.querySelector("#sobre");
+    divhabilidades = document.querySelector("#habilidades");
+    divformacao = document.querySelector("#formacao");
+    divhobbies = document.querySelector("#hobbies");
+    divbooks = document.querySelector("#books");
+
+    divsobre.style.display = "none";
+    divhabilidades.style.display = "none";
+    divformacao.style.display = "none";
+    divhobbies.style.display = "block";
+    divbooks.style.display = "none";
+}
+
+var exibirbooks = function(){
+    divsobre = document.querySelector("#sobre");
+    divhabilidades = document.querySelector("#habilidades");
+    divformacao = document.querySelector("#formacao");
+    divhobbies = document.querySelector("#hobbies");
+    divbooks = document.querySelector("#books");
+
+    divsobre.style.display = "none";
+    divhabilidades.style.display = "none";
+    divformacao.style.display = "none";
+    divhobbies.style.display = "none";
+    divbooks.style.display = "block";
+}
+
